7fe386b546a31b363ab28ea5ed4206ac3c018679
jananukey <janan@ukey.co>
2024-04-02 16:24:31 -0400

    added folder for stores and API

---
+++ src/api/user.ts
@@ -0,0 +1,1 @@
+// here we will have the API routes for the user, must be initialized in the backend first
---
+++ src/stores/user.ts
@@ -0,0 +1,4 @@
+// Here we will code the user store. Global state for the user, using Pinia
+import { defineStore } from 'pinia'
+
+
